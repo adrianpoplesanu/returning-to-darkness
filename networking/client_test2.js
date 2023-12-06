8b92267a32e2ae23fe77bb9148e3313d6e785799
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-12-06 14:46:33 +0200

    server and client back and forth for room creation

--- networking/client_test2.js
+++ networking/client_test2.js
@@ -22,21 +22,37 @@ function initializeConnection(socket) {
 
 function createRoom(socket) {
     console.log("creating room");
-    const payload = {"state": "CREATE_ROOM", "code": "aaa"};
+    const payload = {"state": "CREATE_ROOM", "code": generateUUID4()};
     socket.send(JSON.stringify(payload));
 }
 
-function sendPlayerPosition(socket, data) {
-    const payload = {"state": "UPDATE", "code": "aaa", x: 4, y: 5};
+function sendPlayerPosition(socket, code, data) {
+    const payload = {"state": "UPDATE", "code": code, "data": data};
     socket.send(JSON.stringify(payload));
 }
 
-function joinRoom(socket) {
+function joinRoom(socket, code) {
     console.log("joining room");
-    const payload = {"state": "JOIN_ROOM", "code": "aaa"};
-    socket.senf(JSON.stringify(payload));
+    console.log(code);
+    const payload = {"state": "JOIN_ROOM", "code": code};
+    socket.send(JSON.stringify(payload));
 }
 
 function handleMessage(message) {
     const data = JSON.parse(message.toString());
+    if (data.state == 'ROOM_CREATED') {
+        console.log(data.code);
+    }
+    if (data.state == 'GAME_START') {
+        //... start the game
+    }
+    if (data.state == 'UPDATE') {
+        //... update game state
+    }
+}
+
+function generateUUID4() {
+    let u = Date.now().toString(16) + Math.random().toString(16) + '0'.repeat(16);
+    let guid = [u.substr(0,8), u.substr(8,4), '4000-8' + u.substr(13,3), u.substr(16,12)].join('-');
+    return guid;
 }
--- networking/server_test2.js
+++ networking/server_test2.js
@@ -1,8 +1,9 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 9090 });
 
-connections = [];
-rooms = {};
+var connections = [];
+var connections_map = {};
+var rooms = {};
 
 wss.on('connection', ws => {
     ws.on('message', message => {
@@ -18,20 +19,50 @@ function handleMessage(ws, message) {
     const data = JSON.parse(message.toString());
     if (data.state == 'INIT') {
         console.log("init message received");
+        const id = connections.length;
+        connections.push(ws);
+        ws.id = id;
+        connections_map[id] = ws;
+        console.log(ws.id);
     }
     if (data.state == 'CREATE_ROOM') {
-        console.log("create room message received");
+        console.log("create room[" + data.code + "] message received");
+        handle_create_room(ws, data.code);
+        const payload = {"state": "ROOM_CREATED", "code": data.code};
+        sendMessage(ws, JSON.stringify(payload));
     }
     if (data.state == 'JOIN_ROOM') {
-        console.log("join room message received");
+        console.log("join room[" + data.code + "] message received");
+        handle_join_room(ws, data.code);
     }
     if (data.state == 'UPDATE') {
-        console.log("update player stte message received");
+        handle_update_room(ws, data);
     }
 }
 
 function handleClose(ws) {
     console.log("closing connection");
+    console.log(ws.id);
+    connections.splice(ws.id, 1);
+    delete connections_map[ws.id];
+}
+
+function handle_create_room(ws, code) {
+    console.log("room code generated: " + code);
+    rooms[code] = {
+        player1_ws: ws
+    };
+}
+
+function handle_join_room(ws, code) {
+    rooms[code].player2_ws = ws;
+    const payload = {"state": "GAME_START", "code": code};
+    sendMessage(rooms[code].player1_ws, JSON.stringify(payload));
+    sendMessage(rooms[code].player2_ws, JSON.stringify(payload));
+}
+
+function handle_update_room(ws, data) {
+    console.log(data);
 }
 
 function sendMessage(ws, message) {
