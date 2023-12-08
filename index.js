876eb7d917ca4b876373159353c8930884914cf3
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-12-08 14:15:39 +0200

    minor cleanup

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 $(document).ready(function () {
     $("#create-room-btn").on('click', function () {
-        console.log("create room btn");
         createRoom(socket);
         $('.main-menu').hide();
         $('.room-created-menu').show();
     });
 
     $("#join-room-btn").on('click', function () {
-        console.log("join room btn");
         $('.main-menu').hide();
         $('.join-room-menu').show();
     });
 
     $("#how-to-play-btn").on('click', function () {
-        console.log("how to play btn");
+        //...
     });
 
     $("#join-btn").on('click', function () {
-        console.log("join btn");
         joinRoom(socket, $("#room-code").val());
     });
 
     $("#back-btn").on('click', function () {
-        console.log("back btn");
         $('.join-room-menu').hide();
         $("#room-code").val('');
         $('.main-menu').show();
